882658d24ca54f2c5ca2465464529129a0d8c24c
roy david dueñas alvarez <roy_david0701@hotmail.com>
2018-01-13 23:22:21 -0600

    inicio geo

---
+++ AppMovil5/www/scripts/index.js
@@ -0,0 +1,65 @@
+﻿// Si quiere una introducción sobre la plantilla En blanco, vea la siguiente documentación:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Para depurar código al cargar la página en dispositivos/emuladores Ripple o Android: inicie la aplicación, establezca puntos de interrupción 
+// y ejecute "window.location.reload()" en la Consola de JavaScript.
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Controlar la pausa de Cordova y reanudar eventos
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        //TODO PERSONALIZADO
+        $('#btnMostrarMapa').click(getDatos);
+    };
+
+    function onPause() {
+        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
+    };
+
+    function onResume() {
+        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
+    };
+
+    function getDatos() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+            maximunAge: 300000,
+            timeout: 10000,
+            enableHighAccuracy: true
+        });
+    }
+
+    function onSuccess(position) {
+        var cusLat = position.coords.latitude;
+        var cusLon = position.coords.longitude;
+
+        document.getElementById("txtLat").value = cusLat;
+        document.getElementById("txtLon").value = cusLon;
+
+        try {
+            var coords = new google.maps.LatLng(cusLat, cusLon);
+
+            var opciones = {
+                center: coords, zoom: 15
+            };
+
+            var mapa = new google.maps.Map(document.getElementById("map"), opciones);
+            var marcador = new google.maps.Marker({
+                position: coords,
+                map: mapa,
+                title: "Mi ubicación",
+                animation: google.maps.Animation.DROP
+            });
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    function onError(err) {
+        console.log("codigo de err:" + err.code + "  msj=" + err.message);
+    }
+})();
